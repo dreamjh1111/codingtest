8c82154bf92c9e05c3e30739f8af048aeefccfd0
dreamjh1111 <dreamjh1111@gmail.com>
2021-10-27 10:44:06 +0000

    update

--- gitsetting.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# git init
-git config --global user.email "dreamjh1111@gmail.com"
-git config --global user.name "dreamjh1111"
-#touch readme.md
-#git add readme.md
-#git commit -m "first commit"
